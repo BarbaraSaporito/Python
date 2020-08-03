a36e507afd31c2ccfd29b838b865e7655d75ba7d
Barbara Saporito <66287896+BarbaraSaporito@users.noreply.github.com>
2020-08-03 15:56:35 -0300

    Delete exe034.py

--- exe034.py
+++
@@ -1,6 +0,0 @@
-salario = int(input('Qual o valor do seu salário: '))
-if salario <= 1250:
-    aum = (15 * salario / 100) + salario
-else:
-    aum = (10 * salario / 100) + salario
-print('Seu novo salário com aumento é de \033[7;31;40mR${:.2f}\033[m'.format(aum))
