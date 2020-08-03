0571809f171723155b7de33aafa582c2beb3009b
Barbara Saporito <66287896+BarbaraSaporito@users.noreply.github.com>
2020-08-03 15:29:04 -0300

    Delete exe012.py

--- exe012.py
+++
@@ -1,6 +0,0 @@
-valor = float(input('Qual o valor do produto? '))
-desc = valor - (valor * 5 / 100)
-print('O produto que custava R${}, na promoção com desconto de 5% vai custar R${}'.format(valor, desc))
-
-
-
